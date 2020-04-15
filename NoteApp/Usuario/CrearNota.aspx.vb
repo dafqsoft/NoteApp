2a0f2a1e4924cfec2765d6533681a2931716e2f2
@XBlackGhostX <mariarosacambero11@gmail.com>
2020-04-15 12:56:23 +0100

    Cambiado backend de formulario para guardar una nota de prueba.

--- NoteApp/Usuario/CrearNota.aspx.vb
+++ NoteApp/Usuario/CrearNota.aspx.vb
@@ -1,4 +1,6 @@
-﻿Public Class CrearNota
+﻿Imports NoteApp
+
+Public Class CrearNota
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -8,8 +10,31 @@
         'Agregar.Visible = True
 
         If IsPostBack Then
-            prueba.Text = TituloInput.Text
-            prueba.Text += "<br/>" + TextoContenedor.InnerText.ToString
+            Dim myNote As New Nota
+
+            myNote.Note_Titulo = TituloInput.Text
+            myNote.Note_Contenido = TextoContenedor.InnerText.ToString
+            myNote.Note_FechaAviso = "2020-04-15"
+            myNote.Note_FechaRepetir = "2020-05-10"
+            myNote.Note_Prioridad = "Comun"
+            myNote.Note_CategoriaId = 5
+            myNote.Note_UsuarioId = 2
+            myNote.Note_Avisar = 0
+            myNote.Note_TiempoRepeticion = 0
+
+
+            Dim myAccess As New AccessBase
+
+            If myAccess.editData(myNote) Then
+                prueba.Text = "La nota con titulo: " & TituloInput.Text & " ; y contenido: "
+                prueba.Text += "<br/>" + TextoContenedor.InnerText.ToString & " <br/> Se guardó corréctamente."
+                prueba.ForeColor = Drawing.Color.Green
+            Else
+                prueba.Text = "La nota con titulo: " & TituloInput.Text & " no se guardó bien"
+                prueba.ForeColor = Drawing.Color.Red
+
+            End If
+
         End If
 
     End Sub
